0e3931e0a8b9cc92ac8bc1997df71d0b6b600c15
ashakoor <60638768+ashakoor@users.noreply.github.com>
2020-06-06 13:29:42 -0400

    clean-up of project and solution configurations

--- UnlimitLord/Properties/AssemblyInfo.cs
+++ UnlimitLord/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("55a71128-7708-4cb9-838a-328682885ae3")]
+[assembly: Guid("03256707-ff9b-40f5-b303-40e477333d9e")]
 
 // Version information for an assembly consists of the following four values:
 //
