ce4bd6999bee091f5becba61f47a8a13aab54642
ashakoor <60638768+ashakoor@users.noreply.github.com>
2020-06-06 21:17:19 -0400

    infinite morale!

---
+++ UnlimitLord/PartyMoraleOverride.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using TaleWorlds.CampaignSystem;
+using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
+using TaleWorlds.Localization;
+
+namespace UnlimitLord
+{
+    [HarmonyPatch(typeof(DefaultPartyMoraleModel), "GetEffectivePartyMorale")]
+    internal static class PartyMoraleOverride
+    {
+        public static float Postfix(float result, MobileParty mobileParty, StatExplainer explanation = null)
+        {
+            if (!(mobileParty.Leader == CharacterObject.PlayerCharacter || mobileParty.LeaderHero?.Clan == Clan.PlayerClan))
+                return result;
+
+            var explainedNumber = new ExplainedNumber(0.0f, explanation);
+
+            var textObject = new TextObject("UnlimitLord");
+
+            explainedNumber.Add(100f, textObject);
+
+            return explainedNumber.ResultNumber;
+        }
+    }
+}
--- UnlimitLord/PartySizeLimitOverride.cs
+++ UnlimitLord/PartySizeLimitOverride.cs
@@ -1,15 +1,25 @@
 ﻿using HarmonyLib;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.SandBox.GameComponents.Party;
+using TaleWorlds.Localization;
 
 namespace UnlimitLord
 {
     [HarmonyPatch(typeof(DefaultPartySizeLimitModel), "GetPartyMemberSizeLimit")]
     internal static class PartySizeLimitOverride
     {
-        public static int Postfix(int result, PartyBase party)
+        public static int Postfix(int result, PartyBase party, StatExplainer explanation)
         {
-            return party.MobileParty.IsMainParty && party.LeaderHero.IsHumanPlayerCharacter && !party.MobileParty.IsGarrison ? 100000 : result;
+            if (!(party.MobileParty.IsMainParty && party.LeaderHero.IsHumanPlayerCharacter && !party.MobileParty.IsGarrison))
+                return result;
+
+            var explainedNumber = new ExplainedNumber(0.0f, explanation);
+
+            var textObject = new TextObject("UnlimitLord");
+
+            explainedNumber.Add(100000f, textObject);
+
+            return (int)explainedNumber.ResultNumber;
         }
     }
 }
